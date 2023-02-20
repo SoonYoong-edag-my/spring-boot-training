45c7b347f6bb10db589640ff052c4a339b4e8013
soon-yoong <soon.yoon-ooi@edag.com.my>
2023-02-20 11:09:32 +0800

    Added basic logging

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.Student;
 import com.example.demo.service.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/students")
+@Slf4j
 public class StudentController {
 
     @Autowired
@@ -17,6 +19,7 @@ public class StudentController {
 
     @GetMapping("/ping")
     public String ping() {
+        log.info("ping received");
         return "pong";
     }
 
